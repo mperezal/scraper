928b2e4d8379211dfa7873fc575636d934b2d39a
mperezal <87032627+mperezal@users.noreply.github.com>
2024-07-28 09:43:38 +0200

    Primer código

---
+++ index.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({
+    headless:false,
+    defaultViewport:{
+        width: 19;
+
+    }
+        
+    });
+})();
